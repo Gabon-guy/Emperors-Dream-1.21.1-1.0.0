9345705979c83123b291fc4b24ea229048e88345
Brandon <minecraftcreeperpro123@gmail.com>
2024-10-27 15:11:39 -0700

    e

--- src/main/java/net/gabon_guy/emperorsdream/item/ModItems.java
+++ src/main/java/net/gabon_guy/emperorsdream/item/ModItems.java
@@ -1,4 +1,27 @@
 package net.gabon_guy.emperorsdream.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.gabon_guy.emperorsdream.EmperorsDream;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+    public static final Item TITANIUM = registerItem("titanium", new Item(new Item.Settings()));
+    public static final Item RAW_TITANIUM = registerItem("raw_titanium", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(EmperorsDream.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        EmperorsDream.LOGGER.info("Registering Mod Items for " + EmperorsDream.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(TITANIUM);
+            entries.add(RAW_TITANIUM);
+        });
+    }
 }
