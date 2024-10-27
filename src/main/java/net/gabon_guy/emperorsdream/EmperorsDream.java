ce30f7da04a5d3483057cc42d038b58800f2915d
Brandon <minecraftcreeperpro123@gmail.com>
2024-10-26 18:07:09 -0700

    Setup Code

---
+++ src/main/java/net/gabon_guy/emperorsdream/EmperorsDream.java
@@ -0,0 +1,16 @@
+package net.gabon_guy.emperorsdream;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmperorsDream implements ModInitializer {
+	public static final String MOD_ID = "emperorsdream";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/gabon_guy/emperorsdream/EmperorsDreamClient.java
@@ -0,0 +1,10 @@
+package net.gabon_guy.emperorsdream;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmperorsDreamClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
